a947a3a9c0c62465c95f5e5ceda1ede2210a9357
Pranay Ray <codwotin@gmail.com>
2016-02-16 19:29:55 +0530

    footer.php

---
+++ footer.php
@@ -0,0 +1,56 @@
+<html>
+<head>
+<!-- Bootstrap core CSS -->
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.1/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="style.css" rel="stylesheet">
+	<link href='https://fonts.googleapis.com/css?family=Roboto:300' rel='stylesheet' type='text/css'>
+
+  <style>
+#wrap {
+  min-height: 100%;
+  text-align:center;
+  height: auto;
+  /* Negative indent footer by its height */
+  margin: 0 auto -60px;
+  /* Pad bottom by footer height */
+  padding: 100px 0 60px;
+}
+#footer {
+	left:0;
+	right:0;
+  overflow:hidden;
+  width: 100%;
+  padding: 0 ;
+  height: 60px;
+  background-color: #f5f5f5;
+}
+
+
+/* Custom page CSS
+-------------------------------------------------- */
+/* Not required for template or sticky footer method. */
+
+
+.container{
+	padding :0;
+	text-align:center;
+	margin-left:75px;
+	margin-right:75px;
+	margin-top:10px;
+}
+
+p{
+	font-size:15px;
+	font-family: 'Roboto', sans-serif;
+}</style>
+</head><body>
+  
+ <div id="footer">
+      <div class="container">
+        <p class="text-muted credit">Made with <span class="glyphicon glyphicon-heart" aria-hidden="true"></span> by <a href="pranay.php">Pranay Ray</a></p>
+      </div>
+    </div>
+	</body>
+	</html>
